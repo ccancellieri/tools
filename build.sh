e2736a8768ff8fc70c728c496b7139a51e632283
ccancellieri <carlo.cancellieri@geo-solutions.it>
2012-01-09 13:21:21 +0100

    fixed url in pom for SCM plugin

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true  -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -Dall
+mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true  -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -Dall $1
